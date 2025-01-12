a403988d8d2d1017fdb58b0511cbc9e8af585db2
lahiruransara <lahiruransara123@gmail.com>
2025-01-12 14:46:06 -0800

    Set up base structure and resources

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      Coursera Meta Front-End Developer Professional Certificate Capstone
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
-};
+}
 
 export default App;
---
+++ src/components/Main.test.js
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react';
+import Main from './Main';
+test('renders main', () => {
+  render(<Main />);
+});
