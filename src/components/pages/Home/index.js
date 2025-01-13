35479af297f82e4a2fa9e00ec2523a7b17a70c75
lahiruransara <lahiruransara123@gmail.com>
2025-01-12 16:46:21 -0800

    Add table reservation feature

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,15 @@
+import Hero from './Hero';
+import WeekSpecials from './WeekSpecials';
+import Testimonials from './Testimonials';
+import OurStory from './OurStory';
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WeekSpecials />
+      <Testimonials />
+      <OurStory />
+    </>
+  );
+};
+export default Home;
